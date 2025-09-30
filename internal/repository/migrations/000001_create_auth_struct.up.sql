f4ce0c49fc363cf1be252026fd8fd4e6a1843b7c
Linares <akuruma025@yandex.ru>
2025-09-30 20:42:11 +0300

    delete extension

--- internal/repository/migrations/000001_create_auth_struct.up.sql
+++ internal/repository/migrations/000001_create_auth_struct.up.sql
@@ -1,7 +1,5 @@
-CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
-
 CREATE TABLE "user" (
-  "id" UUID PRIMARY KEY DEFAULT (uuid_generate_v4()),
+  "id" UUID PRIMARY KEY DEFAULT (gen_random_uuid()),
   "bank_user_id" text NOT NULL,
   "created_at" timestamp NOT NULL DEFAULT (now()),
   "updated_at" timestamp
